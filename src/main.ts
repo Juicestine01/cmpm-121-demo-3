6a0e6ce7e4cde6715574e488d047ee8a45ed428c
Justin Xu <jxu240@ucsc.edu>
2024-11-10 00:53:22 -0800

    finished L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 // todo
+const app = document.querySelector<HTMLDivElement>("#app")!;
+const button = document.createElement("button");
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+button.innerHTML = "click me!";
+app.append(button);
