684c205ebad41858e283deb343ce77350f630049
Justin Xu <jxu240@ucsc.edu>
2024-11-13 01:27:38 -0800

    D3a complete

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
-// todo
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// Fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Deterministic random number generator
+import luck from "./luck.ts";
+
 const app = document.querySelector<HTMLDivElement>("#app")!;
 const button = document.createElement("button");
 button.addEventListener("click", () => {
@@ -6,3 +18,100 @@ button.addEventListener("click", () => {
 });
 button.innerHTML = "click me!";
 app.append(button);
+
+// Location of our classroom (as identified on Google Maps)
+const playerLocation = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+// Tunable gameplay parameters
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+// Create the map (element with id "map" is defined in index.html)
+const map = leaflet.map(document.getElementById("map")!, {
+  center: playerLocation,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+// Populate the map with a background tile layer
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+// Add a marker to represent the player
+const playerMarker = leaflet.marker(playerLocation);
+playerMarker.addTo(map);
+
+let coins = 0;
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
+statusPanel.innerHTML = "Num Coins: 0";
+
+// Add caches to the map by cell numbers
+function spawnCache(i: number, j: number) {
+  // Convert cell numbers into lat/lng bounds
+  const origin = playerLocation;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  // Add a rectangle to the map to represent the cache
+  const rect = leaflet.rectangle(bounds).addTo(map);
+
+  // Handle interactions with the cache
+  rect.bindPopup(() => {
+    // Each cache has a random point value, mutable by the player
+    let numCoins = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+
+    // The popup offers a description and button
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+                <div>There is a cache here at "${i},${j}". It has value <span id="value">${numCoins}</span>.</div>
+                <button id="collect">collect</button> <button id="deposit">deposit</button>`;
+
+    function collect() {
+      numCoins--;
+      popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = numCoins
+        .toString();
+      coins++;
+      statusPanel.innerHTML = `Num Coins: ${coins}`;
+    }
+
+    function deposit() {
+      numCoins++;
+      popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = numCoins
+        .toString();
+      coins--;
+      statusPanel.innerHTML = `Num Coins: ${coins}`;
+    }
+
+    // Clicking the button decrements the cache's value and increments the player's points
+    popupDiv
+      .querySelector<HTMLButtonElement>("#collect")!
+      .addEventListener("click", () => {
+        collect();
+      });
+    popupDiv.querySelector<HTMLButtonElement>("#deposit")!
+      .addEventListener("click", () => {
+        deposit();
+      });
+    return popupDiv;
+  });
+}
+
+// Look around the player's neighborhood for caches to spawn
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    // If location i,j is lucky enough, spawn a cache!
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
+}
